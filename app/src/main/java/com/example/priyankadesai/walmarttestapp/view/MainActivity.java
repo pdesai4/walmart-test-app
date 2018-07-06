a9816636a7ba64f3497c3c2280cfcbaf2c844166
Priyanka Desai <pdesai4@binghamton.edu>
2018-07-06 15:14:46 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/priyankadesai/walmarttestapp/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.priyankadesai.walmarttestapp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.priyankadesai.walmarttestapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
